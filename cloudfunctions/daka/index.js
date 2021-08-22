1ab73a2ac761a6532cb1b5509a5fb1fa51e2dc43
Orange <961376413@qq.com>
2021-08-22 14:28:41 +0800

    实现了打卡功能，可以知道菜品对应的打卡人数

--- cloudfunctions/daka/index.js
+++ cloudfunctions/daka/index.js
@@ -1,5 +1,5 @@
 // 云函数入口文件
-const cloud = require('../daka/node_modules/wx-server-sdk')
+const cloud = require('wx-server-sdk')
 
 cloud.init({
   env:'hitsz-recipe-formal-1cosd953108c'
@@ -10,7 +10,9 @@ exports.main = async (event, context) => {
   return await cloud.database().collection(event.canteen).
   doc(event.dangkou).update({
     data:{
-    ['dangkou.meal.'+event.meal_id+'.pj']:event.pj
+    ['dangkou.meal.'+event.meal_id+'.daka']:event.daka,
+    ['dangkou.meal.'+event.meal_id+'.daka_num']:event.daka_num,
+    ['dangkou.meal.'+event.meal_id+'.daka_people']:event.daka_people
     }
   })
 }
