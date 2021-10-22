c5e7e0786447e9f3158c668f4bc64e35043eabd8
Orange <961376413@qq.com>
2021-10-23 00:33:13 +0800

    授权问题更改

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -22,17 +22,20 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    if(wx.getStorageSync('user')==''){
-      this.setData({
-        hasUserInfo: true
-      })
+    if (app.globalData.userInfo) {
+      this.setUserInfo(app.globalData.userInfo);
     }
-    if (wx.getUserProfile) {
+    if(wx.getStorageSync('user')!=''){
+      this.setData({
+        hasUserInfo: true,
+      })
+      this.setUserInfo(wx.getStorageSync('user'))
+    }
+    if(wx.getUserProfile) {
       this.setData({
         canIUseGetUserProfile: true
       })
     }
-
   },
       //兼容低版本用getUseInfo,推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
   getUserProfile(e) {
@@ -41,20 +44,24 @@ Page({
     wx.getUserProfile({
       desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+        this.setUserInfo(res.userInfo);
       }
     })
-    wx.setStorageSync('user',this.data.userInfo)
-    console.log(wx.getStorageSync('user'))
   },
   getUserInfo(e) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    this.setUserInfo(e.detail.userInfo);
   },
+  setUserInfo: function (userInfo) {
+    if (userInfo != null) {
+      app.globalData.userInfo = userInfo
+      console.log(userInfo)
+      wx.setStorageSync('user', userInfo)//缓存用户信息，用于检验用户是否授权登录
+      this.setData({
+        userInfo: userInfo,
+        hasUserInfo: true
+      })
+      
+    }
+  }
 })
